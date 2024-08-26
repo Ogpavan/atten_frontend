877d88ea673e0892b11a4a12805e38e8ff650a01
Pawan Pal <116160257+Ogpavan@users.noreply.github.com>
2024-08-26 16:45:06 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,9 +9,7 @@ function Home() {
         <Link to ="/login">
         <button className='w-full flex items-center justify-center px-10 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-2 md:text-lg md:px-12 poppins-medium">
             '>Login</button></Link>
-              <Link to ="/register">
-        <button className='w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-2 md:text-lg md:px-10
-            '>Register</button></Link>
+      
         
     </div>
   )
